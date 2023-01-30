bfa5fa444eae0fbf95531b85508f8f39d9157001
Karen <karenannetteking@gmail.com>
2023-01-30 12:05:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,30 +52,25 @@ streamlit.write('The user entered', fruit_choice)
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-#dont run anything past here
-streamlit.stop
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-#Snowflake-related functions
+#snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
-streamlit.dataframe(my_data_rows)
-
-
-
+  
 #add a button to load the fruit
 if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
-  my_cnx.close()
   streamlit.dataframe(my_data_rows)
+  
+ #dont run anything past here
+streamlit.stop
+
 
 #allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
